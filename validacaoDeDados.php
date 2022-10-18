113ed63d2b1158d147d5f9c34971e6f50f73a569
PatriciaSuReis <patriciasuellen05@gmail.com>
2022-10-17 22:13:08 -0300

    teste

--- validacaoDeDados.php
+++ validacaoDeDados.php
@@ -1,63 +1,82 @@
 <?php
 
-if(isset($_POST['enviar'])) {
-//dados do formularios
-$nome = $_POST["nome"];
-$genero = $_POST["gen"];
-$dataNasc = $_POST["data_nasc"];
-$telefone = $_POST["tel"];
-$email = $_POST["email"];
+    if (isset($_POST['enviar'])) {
 
-//erros
-$erros = [];
+        if (is_array($_POST['user'])) {
+            //ARRAY - receber dados do formulario
+            $dadosUser = [];
 
-// SANITIZAR limpeza dos dados
-$nomes = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
-$telefones = filter_input(INPUT_POST, 'tel', FILTER_SANITIZE_NUMBER_INT);
-$emails = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+            //ERROS
+            $erros = [];
 
-//VALIDAÇÂO APOS SANITIZAR
-//validação telefone
-    if (!filter_var($telefones, FILTER_VALIDATE_INT)) {
-        $erros[] = "Telefone inválido!";
+            //Passado dados do formluario
+            $dadosUser['nome'] = $_POST['user']['nome'];
+            $dadosUser['dataNasc'] = $_POST['user']['dataNasc'];
+            $dadosUser['telefone'] = $_POST['user']['telefone'];
+            $dadosUser['email'] = $_POST['user']['email'];
+            $dadosUser['genero'] = $_POST['user']['gen'];
+        
+            //SANITIZAR - limpeza dos dados
+            $dadosUser['nome']  = filter_var($dadosUser['nome'], FILTER_SANITIZE_STRING);
+            $dadosUser['telefone'] = filter_var($dadosUser['telefone'], FILTER_SANITIZE_NUMBER_INT);
+            $dadosUser['email']  = filter_var($dadosUser['email'], FILTER_SANITIZE_EMAIL);
+        
+            //VALIDAÇÂO APÓS SANITIZAR
+            //validação telefone
+                if (!filter_var($dadosUser['telefone'], FILTER_VALIDATE_INT)) {
+                    $erros[] = "Telefone inválido!";
+                }
+            
+            //validação email
+                if (!filter_var($dadosUser['email'], FILTER_VALIDATE_EMAIL)) {
+                    $erros[] = "Email inválido!";
+                }
+            
+            //validação data de nascimmento 
+                $array = explode ('-', $dadosUser['dataNasc']);
+    
+                if (!checkdate($array[0], $array[1], $array[2])) {
+                    $erros[] = "Data inválida!";
+                }
+            
+            //
+                $opcoes_gen = ['Masculino','Feminino','N/I']; //array com as opocoes do formulario
+                if (in_array($dadosUser['genero'], $opcoes_gen)){
+                    $dadosUser['genero'] = filter_var($dadosUser['genero'], FILTER_SANITIZE_STRING); 
+                }
+
+
+            //array erros
+                if (!empty($erros)) {
+                    foreach($erros as $erro) {
+                        echo "<li>$erro</li>";
+                    }
+                } else {
+                    echo "Dados corretos.<br><br>";
+                }
+           
+            //salvando dados no arquivo json
+            $jsonDados = @json_decode(file_get_contents('dadsoUser.json'), true);
+
+            if (!$jsonDados)
+                $jsonDados = [];     
+    
+            if (isset($dadosUser)) {
+                $jsonDados[] = $dadosUser;
+
+                    $file = fopen('dadosUser.json', 'a+');
+                    fwrite($file, json_encode($jsonDados, JSON_PRETTY_PRINT));
+
+                    fclose($file);        
+                }
+                $erro = json_last_error();
+                print_r ($erro);
+
+            }
+            //var_dump($dadosUser);
+            //var_dump($jsonDados);
+
+            
     }
-
-//validação email
-    if (!filter_var($emails, FILTER_VALIDATE_EMAIL)) {
-        $erros[] = "Email inválido!";
-    }
-
-//validação data de nascimmento 
-    $array = explode ('-', $dataNasc);
-
-    if (count($array) == 3) {
-        $dia = $array[0];
-        $mes = $array[1];
-        $ano = $array[2]; 
-    }
-
-    if (checkdate($dia, $mes, $ano)) {
-        echo "Data válida<br>";
-
-    } else {
-        $erros[] = "Data inválida!";
-    }
-
-//array erros
-    if (!empty($erros)) {
-        foreach($erros as $erro) {
-            echo "<li>$erro</li>";
-        }
-    } else {
-        echo "<p>Dados corretos.</p>";
-    }
-
-//Mostra os dados
-echo "<hr>";
-echo "Nome: $nomes <br>";
-echo "Data de nascimento: $dataNasc <br>";
-echo "Telefone: $telefones <br>";
-echo "Email: $emails <br>";
-
-}
 ?>
+
